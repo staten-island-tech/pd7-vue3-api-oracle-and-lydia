b1d76ca006d3b93a6b84d761f2b256d18ecf722e
LydiaZ07 <zhulydia07@gmail.com>
2023-04-19 13:13:27 -0400

    l

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const animal = ref('')
+async function getInfo(){
+  let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
+  let data = await res.json()
+  animal.value = data.results
+}
+onMounted(()=>{
+  getInfo()
+}
+)
+</script>
+
+<style scoped>
+
+</style>
