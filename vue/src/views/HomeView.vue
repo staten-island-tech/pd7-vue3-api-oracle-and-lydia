ca3098b532b28d9c3f00baec71ac9da3b713950f
LydiaZ07 <zhulydia07@gmail.com>
2023-04-30 16:57:30 -0400

    l

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,32 +1,79 @@
 <template>
   <div class="container">
     <h1>Urban Park Ranger Animal Condition </h1>
+    <canvas id="barc"></canvas>
   <div class="animal-container">
 <AnimalCard 
-v-for="(monster, index) in animal" 
-:key="monster.name" 
+v-for="(animal, index) in animal" 
+:key="animal.name" 
 :id="index + 1" 
-:animal="monster"/>
+:animal="animal"/>
 </div>
+<canvas id="piec"></canvas>
   </div>
 </template>
 
 <script setup>
-import{ ref, onMounted } from 'vue'
+import{ ref, onMounted, onBeforeMount } from 'vue'
 import AnimalCard from '../components/AnimalCard.vue'
+import Chart from 'chart.js/auto' 
 const animal = ref('')
+
+function sortData() {
+  const data = []
+  animal.value.forEach((ani) => {
+    const index = data.findIndex(e => e.borough === ani.borough)
+    if (data[index]) {
+      data[index].count++
+    } else {
+      const newObject = {
+        borough: ani.borough,
+        count: 1
+      }
+      data.push(newObject)
+    }
+  })
+  console.log(data)
+
+  
+  const ctx = document.getElementById("barc")
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.map(x => x.borough),
+      datasets: [{
+        label: '# of Animals',
+        data: data.map(x => x.count),
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+  myChart;
+}
+
 async function getInfo(){
   let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
   let data = await res.json()
-  animal.value = data.splice(0,20)
+  animal.value = data.splice(0,21)
   // .splice(0,9)
+  console.log(animal)
+  // animal.value.forEach(x => console.log(x))
+  sortData()
 }
- 
+
+
 onMounted(() => {
   getInfo()
+  
+  console.log(animal)
 })
-
-
 </script>
 
 <style scoped>
