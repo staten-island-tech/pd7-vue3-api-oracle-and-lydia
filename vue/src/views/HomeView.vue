e11bb0f8db0d365a51d57dd8338db1b138d10590
LydiaZ07 <zhulydia07@gmail.com>
2023-04-30 17:58:35 -0400

    l

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -57,15 +57,54 @@ function sortData() {
   })
   myChart;
 }
+function sortData2() {
+  const data = []
+  animal.value.forEach((ani) => {
+    const index = data.findIndex(e => e.animal_condition === ani.animal_condition)
+    if (data[index]) {
+      data[index].count++
+    } else {
+      const newObject = {
+        animal_condition: ani.animal_condition,
+        count: 1
+      }
+      data.push(newObject)
+    }
+  })
+  console.log(data)
+
+  
+  const ctx = document.getElementById("piec")
+  const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: data.map(x => x.animal_condition),
+      datasets: [{
+        label: '# of Animals',
+        data: data.map(x => x.count),
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+  myChart;
+}
 
 async function getInfo(){
   let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
   let data = await res.json()
-  animal.value = data.splice(0,21)
+  animal.value = data.splice(0,40)
   // .splice(0,9)
   console.log(animal)
   // animal.value.forEach(x => console.log(x))
   sortData()
+  sortData2()
 }
 
 
@@ -96,4 +135,5 @@ onMounted(() => {
   width: 90vw;
   background-color: blue;
 }
+
 </style>
